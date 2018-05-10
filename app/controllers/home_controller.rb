0e8eed86dfbe860a3c1c84be0de7f1dfa3c0e90f
Tatiana Morris <tatismorris@gmail.com>
2018-05-10 18:58:25 -0400

    added contacts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,15 @@ class HomeController < ApplicationController
 
     @news = News.new
     @news_all = News.all
-    
+
     @program = Program.new
     @programs = Program.all
 
     @abouts = About.all
     @about = About.new
 
+    @contacts = Contact.all
+    @contact = Contact.new
     render layout: 'cms'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_224740) do
+ActiveRecord::Schema.define(version: 2018_05_10_225228) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "image"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_05_09_224740) do
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
