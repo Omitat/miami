b9771dd33da824c94a20c9c434f7c48d1235a9c4
scott4044 <sspiegel1313@gmail.com>
2018-05-11 17:29:42 -0400

    finished log out button. need to make home/show private

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,24 @@
 class SessionsController < ApplicationController
   def new
-    end
+  end
 
-    def create
-      # @admin = Admin.find_by(email: params[:email]).try(:authenticate, params[:password_digest])
-      if params[:code] && params[:state]
-        session[:admin_id] = params[:code]
-        redirect_to home_show_path
-      else
-        render :new
-      end
-      def destory
+  def create
+    # @admin = Admin.find_by(email: params[:email]).try(:authenticate, params[:password_digest])
+    if params[:code] && params[:state]
+      session[:admin_id] = params[:code]
+      redirect_to home_show_path
+    else
+      render :new
+  end
+end
 
+    def destroy
+      if session[:admin_id]
+        session.destroy
+        respond_to do |format|
+          format.html { redirect_to root_path, notice: 'You have been logged out.' }
+          format.json { head :no_content }
       end
     end
   end
+end
